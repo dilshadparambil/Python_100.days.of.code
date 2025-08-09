98cf8ee35e2d20a641bf0991d47dee460d8219b8
Dilshad P <dilshadkareemparambil@gmail.com>
2025-08-09 20:08:39 +0530

    Add files via upload

---
+++ Project_files/d10.py
@@ -0,0 +1,62 @@
+#calculator
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+calc={
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(logo)
+    flag=True
+    num1 = float(input("What's the first number?: "))
+
+    while flag:
+
+        for symbol in calc:
+            print(symbol)
+
+        op = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        res = calc[op](num1, num2)
+        print(f"{num1} {op} {num2} = {res}")
+        calc_continue = input(
+            f"Type 'y' to continue calculating with {res}, or type 'n' to start a new calculation:").lower()
+
+        if calc_continue=='y':
+            num1=res
+        else:
+            flag=False
+            calculator()
+
+calculator()
