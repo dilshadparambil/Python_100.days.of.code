de4f6c41290f9d779cfe6ed9cd702beb97529525
Dilshad P <dilshadkareemparambil@gmail.com>
2025-08-21 15:25:11 +0530

    added day24 files

---
+++ Intermediate/d24/my_code/d24.py
@@ -0,0 +1,14 @@
+# Mail Merge 
+# You need to download the Input and Output directories from /my_code
+
+with open("./Input/Names/invited_names.txt") as name_file:
+    name_list=name_file.readlines()
+
+with open ("./Input/Letters/starting_letter.txt") as letter_file:
+    contents=letter_file.read()
+    for name in name_list:
+        fixed_name=name.strip()
+        fixed_contents=contents.replace("[name]",fixed_name)
+        print(fixed_contents)
+        with open(f"./Output/ReadyToSend/letter_for_{fixed_name}","w") as output_file:
+            output_file.write(fixed_contents)
